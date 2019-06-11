c68acda3a506e5e3797dcc0ae510ee12a8be3d1a
Jay <jupadhyay1@yahoo.com>
2019-06-10 20:30:33 -0700

    Having an issue, but conceptually works.

--- AdventOfCode2018/Day3/AdventDay3.cpp
+++ AdventOfCode2018/Day3/AdventDay3.cpp
@@ -6,24 +6,31 @@ const int maxSize=1000;
 int main()
 {	char c;
 	ifstream infile;
-	
+	infile.open("C:\\Users\\Jay\\Desktop\\Advent2018\\AdventOfCode2018\\AdventOfCode2018\\Day3\\input.txt");
+	cout << "Heres"<< endl;
 	int num1,num2,height,width,count;
-	array int [maxSize][maxSize]={0};// Assign all to zero;
+	int arr[maxSize][maxSize]={0};// Assign all to zero;
 	string str;
+    infile >> c>> num1>> c >> num1>>c>>num2>> c>> height>>c>> width;
 	while (!infile.eof())
 	{
-		getline(infile,str);
 //#1 @ 265,241: 16x26
-		infile >> c>> num1<< c << num1<<c<<num2<< c<< height<< width;
-		for(x=0; x<height; x++)
+		
+        if(!infile)
+        {   
+            cout << "Error" << endl;
+            exit(3);
+        }
+		for(int x=0; x<height; x++)
 		{
-			for(y=0;y<width;y++)
+			for(int y=0;y<width;y++)
 			{
 				arr[num1+x][num2+y]++;
 			}
 		}
+        infile >> c>> num1>> c >> num1>>c>>num2>> c>> height>>c>> width;
 	}
-	
+	cout << "Here"<< endl;
 	count=0;
 	for(int i=0; i<maxSize;i++)
 	{
@@ -31,8 +38,8 @@ int main()
 		{
 			if (arr[i][x]>=2)
 			{
-			 count++
-			{
+			    count++;
+            }
 		}
 	}
 	cout << count<< " is number of overlapping claims."<< endl;
