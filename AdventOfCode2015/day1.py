4bb1beeaa572ec1dda8c6d4fd8c5d59a55e8164d
jupadhyay1 <jupadhyay1@yahoo.com>
2022-04-12 20:02:54 -0700

    Finished Part2

--- AdventOfCode2015/day1.py
+++ AdventOfCode2015/day1.py
@@ -5,12 +5,18 @@ def main():
     #with open("day1.txt") as f:
     lines = f.readlines()
     counter=0
+    counter2=0
     for line in lines:
+
         for chara in line:
+            counter2+=1
             if(chara == '('):
                 counter+=1
             elif chara == ")":
                 counter-=1
+            if counter==-1:
+                print(counter2)
+                
     print( counter)
 
 if __name__ == "__main__":
